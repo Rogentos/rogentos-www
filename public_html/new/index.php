b480507b133085d1121059d403d8511dfdfe5e04
Victor Nițu <vnitu@ceata.org>
2012-11-29 02:11:18 +0200

    First UX tests on Bootstrap

---
+++ public_html/new/index.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Rogentos UX test</title>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+    
+    <link rel="stylesheet" href="bootstrap/css/bootstrap.min.css" />
+    <link rel="stylesheet" href="style.css" />
+    <script type="text/javascript" src="../fw/GENERAL/js/jquery-1.7.2.min.js"></script>
+    <script type="text/javascript" src="bootstrap/js/bootstrap.min.js"></script>
+
+</head>
+
+<body>
+
+    <div class="container">
+        <div class="navbar navbar-inverse">
+            <div class="navbar-inner">
+                <a href="#" class="brand">Rogentos</a>
+                <ul class="nav pull-right">
+                    <li class="dropdown">
+                        <a class="dropdown-toggle" data-toggle="dropdown" href="#">
+                            Despre <b class="caret"></b></a>
+                        <ul class="dropdown-menu" role="menu">
+                          <li><a href="#">Proiectul Rogentos</a></li>
+                          <li><a href="#">Echipa</a></li>
+                          <li class="divider"></li>
+                          <li><a href="#">Contact</a></li>
+                        </ul>
+                    </li>
+                    <li><a href="#">Wiki</a></li>
+                    <li><a href="#">Presa</a></li>
+                    <li><a href="#">Galerie</a></li>
+                    <li class="active"><a href="#">Descarca</a></li>
+                    <li><a href="#">Implica-te</a></li>
+                    <li><a href="#">Webchat</a></li>
+                </ul>
+            </div>
+        </div>
+        <div class="header container-fluid">
+            <a href="#" class="header-download btn btn-success">
+                <i class="icon-download-alt icon-white"></i>
+                <b>Descarcă Rogentos GNU/Linux</b><br />
+                <small>Versiunea 1.5 "Kogaion"</small></a>
+        </div>
+
+        <div class="page container-fluid bpink">
+            <div class="row row-fluid">
+                <div class="span12 ocyan">sdfdfs</div>
+            </div>
+            <div class="feed row row-fluid ogreen">
+                fsdfsd
+            </div>
+        </div>
+
+        <div class="footer container-fluid ">
+
+        </div>
+    </div>
+
+</body>
+</html>
